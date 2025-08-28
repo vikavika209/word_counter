34c2d08e2d3915ab1a6ec99aaa17ddc03ca6ae40
User2 <vkagarmanova@inbox.ru>
2025-08-28 19:33:48 +0500

    Initial commit

---
+++ src/main/java/com/auth/word_counter/WordCounter.java
@@ -0,0 +1,8 @@
+package com.auth.word_counter;
+
+public class WordCounter {
+
+    public static void main(String[] args) {
+    }
+
+}
---
+++ src/test/java/com/auth/word_counter/WordCounterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auth.word_counter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordCounterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
